7db00aca2d442efdf0d68c2676aa31281ed0d1b8
Josh <joshbarr7@gmail.com>
2020-08-25 20:21:52 -0700

    Created basic CRUD api for the client to interact with

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+require("dotenv/config");
+
+app.use(bodyParser.json());
+
+//Import routes
+const linksRoute = require("./routes/links");
+
+app.use("/", linksRoute);
+
+mongoose
+  .connect(process.env.DB_CONNECTION, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .catch((error) => console.log(error));
+app.listen(3000);
